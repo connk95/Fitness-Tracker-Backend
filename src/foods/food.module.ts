ed730b9f49c830fa8f0caf28ef8e5f405aaf87cc
Connor Ketcheson <connorketcheson01@gmail.com>
2024-06-26 12:55:09 +0900

    add forwardref, connect to frontend

--- src/foods/food.module.ts
+++ src/foods/food.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { FoodSchema } from './food.model';
 import { FoodsController } from './food.controller';
 import { FoodsService } from './food.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Foods', schema: FoodSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Foods', schema: FoodSchema }]),
+    forwardRef(() => UsersModule),
+  ],
   controllers: [FoodsController],
   providers: [FoodsService],
   exports: [FoodsService],
--- src/workouts/workouts.module.ts
+++ src/workouts/workouts.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { WorkoutSchema } from './workout.model';
 import { WorkoutsController } from './workouts.controller';
 import { WorkoutsService } from './workouts.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Workouts', schema: WorkoutSchema }]),
+    forwardRef(() => UsersModule),
+    // forwardRef(() => )
   ],
   controllers: [WorkoutsController],
   providers: [WorkoutsService],
