150ea4663cc43672f3e6e926051f865a2ef98151
Connor Ketcheson <connorketcheson01@gmail.com>
2024-11-23 10:35:32 +0900

    edit main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,17 +13,18 @@ async function bootstrap() {
   });
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header(
+    res.setHeader('Access-Control-Allow-Origin', process.env.NETLIFY_URL);
+    res.setHeader(
       'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE',
     );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-      return res.status(200).end();
-    }
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers',
+    );
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Private-Network', true);
+    res.setHeader('Access-Control-Max-Age', 7200);
 
     next();
   });
